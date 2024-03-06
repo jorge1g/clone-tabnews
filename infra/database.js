fdda0b8acfdbfeed914633f40898fe174659227b
Jorge Gonzaga <54822837+jorge1g@users.noreply.github.com>
2024-03-06 14:35:53 +0000

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await cliente.connect();
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await cliente.connect();
     const resultado = await cliente.query(queryObject);
     return resultado;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await cliente.end();
   }
